5d12e2dabf6d28e53ff8ea59a71476b91e430e12
Simon Helleman <shellema@uwo.ca>
2021-04-30 12:24:30 -0400

    Revert back to a simple triangle and changed vertex shader to output color to the fragment shader

--- OpenGL-Book/Main.cpp
+++ OpenGL-Book/Main.cpp
@@ -14,13 +14,14 @@ static int windowHeight = 600;
 
 static const char* vertexShaderSource = "#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
+	"out vec4 vertexColor;\n"
 	"void main()\n"
 	"{\n"
 	"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"	vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
 	"}\0";
 
-static const char* fragmentShaderSource1 = "#version 330 core\nout vec4 FragColor;\nvoid main()\n{\nFragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n}\n";
-static const char* fragmentShaderSource2 = "#version 330 core\nout vec4 FragColor;\nvoid main()\n{\nFragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n}\n";
+static const char* fragmentShaderSource = "#version 330 core\nout vec4 FragColor;\nin vec4 vertexColor;\nvoid main()\n{\nFragColor = vertexColor;\n}\n";
 
 // Called whenever the window is resized -> changes fields and sets viewport
 void FramebufferSizeCallback(GLFWwindow* window, int width, int height)
@@ -41,6 +42,11 @@ void ProcessInput(GLFWwindow* window)
 	{
 		glfwSetWindowShouldClose(window, GL_TRUE);
 	}
+	
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+	{
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	}
 }
 
 int main()
@@ -52,6 +58,7 @@ int main()
 		return -1;
 	}
 
+	std::cout << "glfwInit() complete" << std::endl;
 	// Set OpenGL version and set to core mode (modern)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -84,90 +91,34 @@ int main()
 	glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
 	
 	
-	float triangle1[] = { 
-		-0.9f, -0.5f, 0.0f,
-		0.0f, -0.5f, 0.0f,
-		-0.45f, 0.5f, 0.0f
-	};
-
-	float triangle2[] = {
-		0.0f, -0.5f, 0.0f,
-		0.9f, -0.5f, 0.0f,
-		0.45f, 0.5f, 0.0f
-	};
-
-	unsigned int indices[] = {  // note that we start from 0!
-		0, 1, 2, // First triangle
-		3, 2, 1
+	float triangle[] = { 
+		-0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f
 	};
 
 	// VBO = Vertex Buffer Object -> verticies we want to store in the GPU's memory
 
 	// Create a buffer object, there are several different types of buffer objects
 	// OpenGL allows us to bind several buffers at once as long as they have a different buffer type
-	unsigned int triangle1VBO;
-	glGenBuffers(1, &triangle1VBO);
-
-	unsigned int triangle2VBO;
-	glGenBuffers(1, &triangle2VBO);
+	unsigned int triangleVBO;
+	glGenBuffers(1, &triangleVBO);
 
 
 	// VAO = Vertex Array Object -> Once bound any subsquent vertex attribute calls from that point will be stored inside the VAO
 	//		which makes is simple to switch objects (just switch VAO)
-	unsigned int triangle1VAO;
-	glGenVertexArrays(1, &triangle1VAO);
-
-	unsigned int triangle2VAO;
-	glGenVertexArrays(1, &triangle2VAO);
-
-	// EBO = Element Array Object -> Stores indincies which OpenGL uses to decide what verticies to draw
-	// unsigned int EBO;
-	// glGenBuffers(1, &EBO);
-	
+	unsigned int triangleVAO;
+	glGenVertexArrays(1, &triangleVAO);
 
 	// 1. Bind vertex array object
-	glBindVertexArray(triangle1VAO);
+	glBindVertexArray(triangleVAO);
 
 	// 2. Copy verticies into a buffer for OpenGL to use
-	glBindBuffer(GL_ARRAY_BUFFER, triangle1VBO); // From this point, any calls to glBufferData() will be on the currently bound buffer (VBO)
+	glBindBuffer(GL_ARRAY_BUFFER, triangleVBO); // From this point, any calls to glBufferData() will be on the currently bound buffer (VBO)
 	
-	// glBufferData() is a function specifically targeted to copy user-defined data into the currently bound buffer.
-	// Its first argument is the type of the buffer we want to copy data into: the vertex buffer object currently bound to the GL_ARRAY_BUFFER target.
-	// The second argument specifies the size of the data (in bytes) we want to pass to the buffer; a simple sizeof of the vertex data suffices.
-	// The third parameter is the actual data we want to send.
-	// The fourth parameter specifies how we want the graphics card to manage the given data. This can take 3 forms:
-	//		GL_STREAM_DRAW: the data is set only onceand used by the GPU at most a few times.
-	//		GL_STATIC_DRAW : the data is set only onceand used many times.
-	//		GL_DYNAMIC_DRAW : the data is changed a lotand used many times.
-	// The position data of the triangle does not change, is used a lot, and stays the same for every render call so its usage type should best be 
-	// GL_STATIC_DRAW.If, for instance, one would have a buffer with data that is likely to change frequently, a usage type of GL_DYNAMIC_DRAW ensures
-	// the graphics card will place the data in memory that allows for faster writes.
-	// As of now we stored the vertex data within memory on the graphics card as managed by a vertex buffer object named VBO.Next we want to create a
-	// vertexand fragment shader that actually processes this data, so let's start building those.
- 	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle1), triangle1, GL_STATIC_DRAW); // Push buffer to GPU
+ 	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW); // Push buffer to GPU
 
-	// 3. Copy index array in an elelment buffer for OpenGL to use
-	// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	// glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	// 4. Set the vertex attributes pointers
-
-	// Tell OpenGL how our vertex buffer data is formatted
-	// Paramater 1:	vertex attribute to configure. In this case we specified the location of the position vertex attribute
-	//				in the in the vertex shader with location = 0.
-	// Paramater 2:	Size of vertex attribute, in this case it is a vec3 which is composed of 3 floats
-	// Paramater 3: Type of the data
-	// Paramater 4: Should the data be normalized (not relavent here)
-	// Paramater 5: Stide = space between consecutive vertex attribs. In this case they are spaced out by 3 floats
-	// Paramater 6: Offset where position data begins in the buffer (0 in this case)
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	// Triangle 2
-	glBindVertexArray(triangle2VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, triangle2VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle2), triangle2, GL_STATIC_DRAW);
-
+	// 3. Set the vertex attributes pointers
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
@@ -177,31 +128,21 @@ int main()
 	glCompileShader(vertexShader);
 
 	// Create fragment shaders -> basically the same as the vertex
-	unsigned int fragmentShader1 = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader1, 1, &fragmentShaderSource1, nullptr);
-	glCompileShader(fragmentShader1);
-
-	unsigned int fragmentShader2 = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader2, 1, &fragmentShaderSource2, nullptr);
-	glCompileShader(fragmentShader2);
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+	glCompileShader(fragmentShader);
 
 	// Create a shader program
 	// When linking the shaders into a program it links the outputs of each shader to the inputs of the next shader.
 	// This is also where you'll get linking errors if your outputs and inputs do not match.
-	unsigned int shaderProgram1 = glCreateProgram();
-	glAttachShader(shaderProgram1, vertexShader);
-	glAttachShader(shaderProgram1, fragmentShader1);
-	glLinkProgram(shaderProgram1);
-	
-	unsigned int shaderProgram2 = glCreateProgram();
-	glAttachShader(shaderProgram2, vertexShader);
-	glAttachShader(shaderProgram2, fragmentShader2);
-	glLinkProgram(shaderProgram2);
+	unsigned int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
 
 	// Delete the shaders since we don't need them anymore
 	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader1);
-	glDeleteShader(fragmentShader2);
+	glDeleteShader(fragmentShader);
 
 #ifdef DISPLAY_WIREFRAME
 	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -214,18 +155,11 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shaderProgram1);
-		glBindVertexArray(triangle1VAO);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(triangleVAO);
+
 		// Numbers represent start index and how many verticies
 		glDrawArrays(GL_TRIANGLES, 0, 3);
-
-		glUseProgram(shaderProgram2);
-		glBindVertexArray(triangle2VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-			
-		// Now with an EBO, we use glDrawElements()
-		// glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		// glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
