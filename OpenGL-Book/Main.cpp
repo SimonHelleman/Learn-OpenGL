c114117c85d40de496479146713f46c9fe61349b
Simon Helleman <shellema@uwo.ca>
2021-04-30 12:53:20 -0400

    Added a unifrom to the fragment shader and made the green component of the color change with time

--- OpenGL-Book/Main.cpp
+++ OpenGL-Book/Main.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -12,16 +13,23 @@ static int windowWidth = 800;
 static int windowHeight = 600;
 #endif
 
-static const char* vertexShaderSource = "#version 330 core\n"
+
+static const char* vertexShaderSource =
+	"#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
-	"out vec4 vertexColor;\n"
 	"void main()\n"
 	"{\n"
-	"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-	"	vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+	"   gl_Position = vec4(aPos, 1.0);\n"
 	"}\0";
 
-static const char* fragmentShaderSource = "#version 330 core\nout vec4 FragColor;\nin vec4 vertexColor;\nvoid main()\n{\nFragColor = vertexColor;\n}\n";
+static const char* fragmentShaderSource =
+	"#version 330 core\n"
+	"out vec4 FragColor;\n"
+	"uniform vec4 ourColor;\n"
+	"void main()\n"
+	"{\n"
+	"	FragColor = ourColor;\n"
+	"}\n";
 
 // Called whenever the window is resized -> changes fields and sets viewport
 void FramebufferSizeCallback(GLFWwindow* window, int width, int height)
@@ -42,7 +50,7 @@ void ProcessInput(GLFWwindow* window)
 	{
 		glfwSetWindowShouldClose(window, GL_TRUE);
 	}
-	
+
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
 	{
 		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -78,20 +86,20 @@ int main()
 	}
 
 	glfwMakeContextCurrent(window);
-	
+
 	// Initialize GLAD -> I can't say I completely understand this but it works like a black box
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cerr << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
-	
+
 	// Set the view port to be the entire window and register the callback to handle resizeing
 	glViewport(0, 0, windowWidth, windowHeight);
 	glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
-	
-	
-	float triangle[] = { 
+
+
+	float triangle[] = {
 		-0.5f, -0.5f, 0.0f,
 		0.0f, 0.5f, 0.0f,
 		0.5f, -0.5f, 0.0f
@@ -115,8 +123,8 @@ int main()
 
 	// 2. Copy verticies into a buffer for OpenGL to use
 	glBindBuffer(GL_ARRAY_BUFFER, triangleVBO); // From this point, any calls to glBufferData() will be on the currently bound buffer (VBO)
-	
- 	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW); // Push buffer to GPU
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW); // Push buffer to GPU
 
 	// 3. Set the vertex attributes pointers
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -156,9 +164,12 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
-		glBindVertexArray(triangleVAO);
+		float timeValue = (float)glfwGetTime();
+		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
-		// Numbers represent start index and how many verticies
+		glBindVertexArray(triangleVAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
